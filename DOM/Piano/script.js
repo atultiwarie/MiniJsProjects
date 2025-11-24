6d78c127582c28fe5273421dd4f5e5c9ccb9f374
atultiwarie <atultiwari4767@gmail.com>
2025-11-24 19:38:39 +0530

    made it functional

---
+++ DOM/Piano/script.js
@@ -0,0 +1,27 @@
+let allKeys= []
+let audio = new Audio('./tunes/tunes/a.wav');
+
+const pianoKeys = document.querySelectorAll('.piano .key');
+
+function playTune(key){
+     audio.pause();
+     audio.currentTime = 0; 
+    audio.src = `./tunes/tunes/${key}.wav`;
+    audio.play();
+    const clickedKey = document.querySelector(`.piano .key[data-key="${key}"]`);
+    clickedKey.classList.add('active');
+    setTimeout(()=>{
+        clickedKey.classList.remove('active');
+    }, 200)
+
+}
+
+pianoKeys.forEach((key)=>{
+    allKeys.push(key.dataset.key);
+    key.addEventListener('click', ()=> playTune(key.dataset.key) )
+});
+
+document.addEventListener('keydown',(e)=>{
+    if(allKeys.includes(e.key)) playTune(e.key)
+
+})
